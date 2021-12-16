b2dd0ca8fb236567ea655fbbfb6af5f28cd6cc92
Dylan Mooers <dmooers@calpoly.edu>
2021-12-15 16:04:33 -0800

    more tests

--- test/index.ts
+++ test/index.ts
@@ -19,29 +19,64 @@ describe("Swap", function () {
     const ERC20 = await ethers.getContractFactory("LPToken");
     const Swap = ethers.getContractFactory("OpenSumSwap");
 
-    token1 = (await ERC20.deploy("Test 1", "t1", "18")) as ERC20_T;
-    token2 = (await ERC20.deploy("Test 2", "t2", "18")) as ERC20_T;
+    console.log(`Signer: ${signer}`);
+
+    token1 = (await ERC20.deploy("wETH", "wETH", "18")) as ERC20_T;
+    token2 = (await ERC20.deploy("wETH", "wETH", "18")) as ERC20_T;
     swapContract = (await (
       await Swap
     ).deploy(
       [token1.address, token2.address],
-      ["8", "8"],
+      ["18", "18"],
       "Test",
       "t"
     )) as SwapContract;
-    token1.mint(signer, "100000000000000000", { from: signer });
-    token2.mint(signer, "100000000000000000", { from: signer });
+    token1.mint(
+      "0x13783136336932a54ae5a5939b237F78AE369036",
+      "23896000000000000000",
+      { from: signer }
+    );
+    token2.mint(
+      "0x13783136336932a54ae5a5939b237F78AE369036",
+      "6971000000000000000",
+      { from: signer }
+    );
+    token1.mint(signer, "100000000000000000000000000000000000", {
+      from: signer,
+    });
+    token2.mint(signer, "100000000000000000000000000000000000", {
+      from: signer,
+    });
+
     lpToken = ERC20.attach(await swapContract.getLpToken()) as ERC20_T;
+
+    console.log(swapContract.address, token1.address, token2.address);
   });
   it("Can be deposited into", async function () {
-    const expectedIncrease = "10000000000000";
+    const expectedIncrease = "1000000000000000000000000000000000";
     const initial = await lpToken.balanceOf(signer);
     const expected = initial.add(expectedIncrease);
 
-    await token1.approve(swapContract.address, "500", { from: signer });
-    await token2.approve(swapContract.address, "500", { from: signer });
-
-    await swapContract.addLiquidity(["500", "500"], "999", { from: signer });
+    await token1.approve(
+      swapContract.address,
+      "500000000000000000000000000000000",
+      { from: signer }
+    );
+    await token2.approve(
+      swapContract.address,
+      "500000000000000000000000000000000",
+      { from: signer }
+    );
+    await swapContract.addLiquidity(
+      [
+        "500000000000000000000000000000000",
+        "500000000000000000000000000000000",
+      ],
+      "999",
+      {
+        from: signer,
+      }
+    );
     const actual = await lpToken.balanceOf(signer);
     expect(actual).equal(expected);
   });
